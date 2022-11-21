c620ce27fa4c41211ef3c73e78bf9021233014e4
fernando <fernando.casta.1103@gmail.com>
2022-11-21 14:09:33 -0600

    error handler para error 404

--- web_personal/appy.py
+++ web_personal/appy.py
@@ -1,3 +1,4 @@
+####from crypt import methods
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -50,6 +51,10 @@ def welcome():
     access = {'email':email, 'password': password}
 
     return render_template('admin/index.html', user_access=access )
+
+@app.errorhandler(404)
+def page_error_not_found(e):
+    return render_template('error/404.html'), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
