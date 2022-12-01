cabead38525fe52f6b5db368e8bc35870458feae
fernando <fernando.casta.1103@gmail.com>
2022-12-01 09:34:47 -0600

    Implementando LoginForm parte 2

--- web_personal/appy.py
+++ web_personal/appy.py
@@ -1,7 +1,8 @@
 #from crypt import methods
-from flask import Flask, render_template, request
+from flask import Flask,redirect, render_template, request, url_for
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, SubmitField, EmailField
+from wtforms.validators import DataRequired, Email
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
@@ -40,14 +41,21 @@ def portfolio():
 ######### Formularios de WTForms ############
 
 class LoginForm(FlaskForm): 
-    username = EmailField('Username')
-    password = PasswordField('Password')
-    submit = SubmitField('Login')
+    email = EmailField('Correo', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Ingresar')
 
 ####### Rutas Login #########
-@app.route('/auth/login')
+@app.route('/auth/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()  
+    form = LoginForm() 
+
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+        access = {'email':email}
+
+        return render_template('admin/index.html', email=email)
     return render_template('auth/login.html', form=form)
 
 @app.route('/auth/register')
@@ -55,12 +63,17 @@ def register():
     return render_template('auth/register.html')
 
 @app.route('/welcome', methods=['GET', 'POST'])
-def welcome():
-    email = request.form['mail']
-    password = request.form['password']
-    access = {'email':email, 'password': password}
+def welcome(form):
+    
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+        access = {'email':email}
 
-    return render_template('admin/index.html', user_access=access )
+        return render_template('admin/index.html', email=email)
+    
+    return redirect(url_for('login'))
+
 
 @app.errorhandler(404)
 def page_error_not_found(e):
